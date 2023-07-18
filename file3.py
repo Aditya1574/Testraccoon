9585af2a332705d76c1f18dffeabb065ea62ccc2
Aditya Singh1 <Aditya.Singh1@Geminisolutions.com>
2023-07-18 12:10:54 +0530

    adding text to speech feature in the main

---
+++ file3.py
@@ -0,0 +1,11 @@
+import requests
+import win32com.client as wincom
+
+speak = wincom.Dispatch("SAPI.SpVoice")
+
+
+
+text = "Python text-to-speech test. using win32com.client"
+speak.Speak(text)
+
+
