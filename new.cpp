98cbbd161ba01e2d495df4886139470de165be60
Aditya Singh1 <Aditya.Singh1@Geminisolutions.com>
2023-07-18 10:35:23 +0530

    this is a second commit

---
+++ new.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int a = 1, b = 2;
+	cout<<"a = "<<a<<"b = "<<b<<endl;
+	a = a + b;
+	b = a - b;
+	a = a - b;
+	cout<<"a = "<<a<<"b = "<<b<<endl;
+	return 0;
+}
+
